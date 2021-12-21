f6622081d3b7aee5590c57e06e66f23a3c4bdeae
raghav-rama <hackerer528@gmail.com>
2021-12-21 15:35:15 +0530

    init

--- SyntheticCode/src/com/company/SyntheticGenericTest.java
+++ SyntheticCode/src/com/company/SyntheticGenericTest.java
@@ -1,5 +1,25 @@
 package com.company;
 
-public class SyntheticGenericTest {
+import java.util.Date;
 
+public class SyntheticGenericTest<A extends CharSequence, B extends Date> {
+
+    public static void main(String[] args) {
+        NestedClass<String, Date> nestedClass = new NestedClass<>();
+        System.out.println(nestedClass.getData());
+    }
+    private static final class NestedClass<A, B> {
+        private A testA;
+        private B testB;
+
+        private Object getData() {
+            return getTestString() + "-" + getTestDate();
+        }
+        public A getTestString() {
+            return testA;
+        }
+        public B getTestDate() {
+            return testB;
+        }
+    }
 }
