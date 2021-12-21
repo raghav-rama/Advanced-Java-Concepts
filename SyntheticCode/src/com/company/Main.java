d00d00ca0f741155093a2f1d4ebead21092a8dc4
raghav-rama <hackerer528@gmail.com>
2021-12-21 20:40:42 +0530

    init

--- SyntheticCode/src/com/company/Main.java
+++ SyntheticCode/src/com/company/Main.java
@@ -1,5 +1,11 @@
 package com.company;
 
+/**
+ * <b>{@code JDK verion 17.0.1}</b><br>
+* THIS PROJECT IS SORT OF A FORK OF THE FOLLOWING GITHUB REPO<br>
+* <a href="https://github.com/root0109/synthetic-concept">Click here to open in browser</a>
+ */
+
 public class Main {
 
     public static void main(String[] args) {
--- SyntheticCode/src/com/company/SyntheticTest.java
+++ SyntheticCode/src/com/company/SyntheticTest.java
@@ -3,6 +3,26 @@ package com.company;
 import java.util.Date;
 
 public class SyntheticTest {
+
+    public static void main(String[] args) {
+        SyntheticTest syntheticTest = new SyntheticTest();
+        NestedClass nestedClass = syntheticTest.new NestedClass();
+        System.out.println(nestedClass.testString);
+        System.out.println(nestedClass.getTestString());
+        System.out.println(nestedClass.getData());
+        System.out.println(nestedClass.getData1());
+    }
+    /**
+     *
+     * <h1>Java 17 vs Java 8</h1>
+     * <h2>Synthetic code emitted after compilation</h2>
+     * If this class is compiled by Java 8, it emits <br>{@code final com.company.SyntheticTest this$0}<br>
+     * {@code static java.lang.String access$1(com.company.SyntheticTest$NestedClass);}<br>
+     * {@code static java.lang.String access$2(com.company.SyntheticTest$NestedClass);}<br>
+     * {@code static java.lang.String access$3(com.company.SyntheticTest$NestedClass);}<br>
+     * whereas if compiled by Java 17 only {@code this$0} is emitted<br>
+     * and less synthetic functions mean more performance vro ;)
+      */
     private final class NestedClass {
         private String testString = "Synthetic Nested Class : ";
         private Date testDate = new Date();
@@ -18,14 +38,5 @@ public class SyntheticTest {
         public Date getTestDate() {
             return testDate;
         }
-    }
-
-    public static void main(String[] args) {
-        SyntheticTest syntheticTest = new SyntheticTest();
-        NestedClass nestedClass = syntheticTest.new NestedClass();
-        System.out.println(nestedClass.testString);
-        System.out.println(nestedClass.getTestString());
-        System.out.println(nestedClass.getData());
-        System.out.println(nestedClass.getData1());
     }
 }
