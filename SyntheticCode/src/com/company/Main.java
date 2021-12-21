d914e85e298848e366a60cf9e6a598866859d79a
raghav-rama <hackerer528@gmail.com>
2021-12-21 15:27:27 +0530

    init

--- SyntheticCode/src/com/company/Main.java
+++ SyntheticCode/src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("this project demonstrates how the compiler implements synthetic code in java");
     }
 }
