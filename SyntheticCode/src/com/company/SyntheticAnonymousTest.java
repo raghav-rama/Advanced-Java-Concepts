ab1ee2ebefdda81bdc2074484e42577963e8303c
raghav-rama <hackerer528@gmail.com>
2021-12-21 14:18:41 +0530

    init

--- SyntheticCode/src/com/company/SyntheticAnonymousTest.java
+++ SyntheticCode/src/com/company/SyntheticAnonymousTest.java
@@ -1,5 +1,19 @@
 package com.company;
+/*
+This class will produce one class of Synthetic code namely 'SyntheticAnonymousTest$1.class'
+ */
 
 public class SyntheticAnonymousTest {
+    interface Callback<T> {
+        String call(T value);
+    }
 
+    public static void main(String... args) {
+        System.out.println(new Callback<String>() {
+            @Override
+            public String call(String value) {
+                return ("Synthetic anonymous "+value);
+            }
+        }.call("Test"));
+    }
 }
