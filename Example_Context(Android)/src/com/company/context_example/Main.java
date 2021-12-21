2ae6963a0f6f74f425d94fd96c8e40cbccb917a9
raghav-rama <hackerer528@gmail.com>
2021-12-21 13:50:47 +0530

    init

--- Example_Context(Android)/src/com/company/context_example/Main.java
+++ Example_Context(Android)/src/com/company/context_example/Main.java
@@ -2,7 +2,7 @@ package com.company.context_example;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         Activity activity = new Activity(new Context() {
             @Override
             public Context getContext() {
