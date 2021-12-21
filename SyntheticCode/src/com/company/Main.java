8146af8f612d4fd9edc8df3dab1d1092d2ba01b3
raghav-rama <hackerer528@gmail.com>
2021-12-21 16:38:47 +0530

    init

--- SyntheticCode/src/com/company/Main.java
+++ SyntheticCode/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("this project demonstrates how the compiler implements synthetic code in java");
+        System.out.println("this project demonstrates how the compiler implements synthetic code in java\n");
+        System.out.println("this project is compiled using jdk-17.0.1");
+        System.out.println("if the code is compiled using java 8, there are more synthetic functions compared to java 17");
+        System.out.println("for example in SyntheticGenericClass, access$1 method is not emitted by the java 17 compiler");
+        System.out.println("also, in SyntheticInnerClass, only this$0 method is emitted by java 17 compiler compared to access$0, access$1 and this$0 by java 8 compiler");
     }
 }
