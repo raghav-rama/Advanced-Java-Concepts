523dbd7ab124e178c7fcaa79b9e14fa99e2c23b8
raghav-rama <hackerer528@gmail.com>
2022-03-31 10:39:31 +0530

    SmallestString

--- General/src/com/company/newtonschool/SmallestString.java
+++ General/src/com/company/newtonschool/SmallestString.java
@@ -6,9 +6,9 @@ public class SmallestString {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int T;
-        StringBuilder str = new StringBuilder("");
+        StringBuilder str = new StringBuilder();
         char a = 'a';
-        int count=2,i=0;
+        int count=2,i;
         T=sc.nextInt();
         int t=T;
         int[] N = new int[T];
@@ -19,34 +19,30 @@ public class SmallestString {
         }
         i=0;
         while(t-- > 0) {
-
             if(N[i]<K[i] | K[i]==1) {
-                if(N[i]==1) {
+                if(N[i]==1 && K[i]==1) {
                     System.out.println(a);
-                    ++i;
-                    continue;
                 } else {
                     System.out.println("-1");
-                    ++i;
-                    continue;
                 }
-            }
-            else {
+                ++i;
+                continue;
+            } else {
                 for(int j=1;j<=N[i];++j) {
-
                     if(j <= (N[i]-(K[i]-2)) ) {
                         if(j%2==0)
                             str.append((char)(a+1));
                         else
-                            str.append((char)(a));
+                            str.append(a);
                     }
-                    else {
+                    else
                         str.append((char)(a+count++));
-                    }
                 }
             }
             System.out.println(str);
             ++i;
+            count=2;
+            str.setLength(0);
         }
     }
 }
