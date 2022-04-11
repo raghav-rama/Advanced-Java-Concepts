331884f7d14e50ed69950595b70652603d6aa850
raghav-rama <hackerer528@gmail.com>
2022-04-11 12:06:55 +0530

    improved using BigInteger

--- General/src/com/company/codechef/APRIL221D/AXNODR.java
+++ General/src/com/company/codechef/APRIL221D/AXNODR.java
@@ -1,18 +1,21 @@
 package com.company.codechef.APRIL221D;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class AXNODR {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T=sc.nextInt();
-        int[] C = new int[T];
+//        int[] C = new int[T];
+        BigInteger[] C = new BigInteger[T];
         for(int i=0; i<T; ++i)
-            C[i]=sc.nextInt();
+            C[i]=new BigInteger(sc.next());
         for(int i=0;i<T;++i) {
-            int B=1;
-            for(int j=2;j<=C[i];++j)
-                B=j%2==0?B^j:B&j;
+            BigInteger B = BigInteger.ONE;
+            for(BigInteger j=BigInteger.TWO;j.compareTo(C[i])<=0;j=j.add(BigInteger.ONE))
+//                B=j%2==0?B^j:B&j;
+                B=j.mod(BigInteger.TWO).compareTo(BigInteger.ZERO) == 0 ? B.xor(j) : B.and(j);
             System.out.println(B);
         }
     }
