c96759c316f00874f2092791088f2787a5edc333
raghav-rama <hackerer528@gmail.com>
2022-04-19 01:19:19 +0530

    init

--- General/src/com/company/codechef/APRIL221D/AXNODR.java
+++ General/src/com/company/codechef/APRIL221D/AXNODR.java
@@ -7,14 +7,16 @@ public class AXNODR {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T=sc.nextInt();
-        BigInteger[] C = new BigInteger[T];
+        long[] C = new long[T];
         for(int i=0; i<T; ++i)
-            C[i] = new BigInteger(sc.next());
+            C[i] = sc.nextLong();
         for(int i=0;i<T;++i) {
-            BigInteger B = BigInteger.ONE;
-            for(BigInteger j = BigInteger.TWO; j.compareTo(C[i]) <= 0; j = j.add(BigInteger.ONE))
-                B = j.mod(BigInteger.TWO).compareTo(BigInteger.ZERO) == 0 ? B.xor(j) : B.and(j);
-            System.out.println(B);
+            if( (C[i]-1) % 4 == 0 )
+                System.out.println(C[i]);
+            else if( C[i] % 4 == 0 )
+                System.out.println(C[i]+3);
+            else
+                System.out.println(3);
         }
     }
 }
